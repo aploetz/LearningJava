57ccfd0eec9e6ee206b36333b7b95d923e1d9e3f
Aaron Ploetz <aaron.ploetz@target.com>
2022-05-31 13:02:26 -0500

    getting FirstProgram to work

---
+++ src/FirstProgram.java
@@ -0,0 +1,12 @@
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		/* Sometimes comments get longer than anticipated
+		   and can run multiple lines.  That’s when using
+		   a block comment is a good idea. */
+
+		System.out.print("I am a computer programmer!");
+	}
+
+}
