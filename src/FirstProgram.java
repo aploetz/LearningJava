166cef34c0aef19da5dd05838889fe4733574bd7
Aaron Ploetz <aaron.ploetz@target.com>
2022-06-04 12:50:12 -0500

    additional ways to implement LF

--- src/FirstProgram.java
+++ src/FirstProgram.java
@@ -6,7 +6,10 @@ public class FirstProgram {
 		   and can run multiple lines.  That’s when using
 		   a block comment is a good idea. */
 
-		System.out.print("I am a computer programmer!" + (char)10);
+		// System.out.print("I am a computer programmer!\n");
+		// System.out.print("I am a computer programmer!" + (char)10);
+		// System.out.print("I am a computer programmer!" + System.lineSeparator());
+		System.out.println("I am a computer programmer!");
 		System.out.print("End of program.");
 
 	}
