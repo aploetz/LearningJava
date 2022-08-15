85bc0fc6708472768dc4b9d0d62e924b488e5444
Aaron Ploetz <aaron.ploetz@target.com>
2022-08-14 19:45:52 -0500

    conditional programs for chapter 5

---
+++ src/chapter5/IfProgram.java
@@ -0,0 +1,18 @@
+package chapter5;
+
+public class IfProgram {
+
+	public static void main(String[] args) {
+		int intA = 16;
+		
+		if (intA == 5) {
+			System.out.println("The variable intA equals 5.");
+		} else if (intA > 10) {
+			System.out.println("The variable intA is greater than 10.");
+		} else {
+			System.out.println("The variable intA is NOT equal to 5.");
+		}
+
+	}
+
+}
---
+++ src/chapter5/SwitchCaseProgram.java
@@ -0,0 +1,61 @@
+package chapter5;
+
+import java.util.Scanner;
+
+public class SwitchCaseProgram {
+
+	public static void main(String[] args) {
+		Scanner inputScanner = new Scanner(System.in);
+		String status;
+
+		System.out.print("Enter a numeric status: ");
+		int input = inputScanner.nextInt();
+		inputScanner.close();
+
+/*		switch(input) {
+			case 2:
+				status = "PAYMENT RECEIVED";
+		break;
+			case 3:
+				status = "INVENTORY PICKED";
+		break;
+			case 4:
+				status = "SHIPPED";
+		break;
+			case 5:
+				status = "DELIVERED";
+		break;
+			default:
+				status = "PENDING";
+		}*/
+
+		status = decodeStatus(input);
+		
+		System.out.printf("Order code of %d equals a status of %s \n", input, status);
+
+	}
+
+	private static String decodeStatus(int statusNum) {
+		
+		String returnVal;
+		
+		switch(statusNum) {
+			case 2:
+				returnVal = "PAYMENT RECEIVED";
+		break;
+			case 3:
+				returnVal = "INVENTORY PICKED";
+		break;
+			case 4:
+				returnVal = "SHIPPED";
+		break;
+			case 5:
+				returnVal = "DELIVERED";
+		break;
+			default:
+				returnVal = "PENDING";
+		}
+		
+		return returnVal;
+	}
+}
